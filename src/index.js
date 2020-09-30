cdd2c0f35cd30da9611127abf9bc129a6b6d3396
TanayZha <tattcy@gmail.com>
2020-10-01 01:04:29 +0300

    feat: resolve typical arrays problrms

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
+
+  if (array && array.length) {
+    
+    var min = Math.min.apply(null, array);
+
+    return  min;
+    
+
+} else {
   return 0;
+}
 }
 
 exports.max = function max (array) {
+  if (array && array.length) {
+    
+    var max = Math.max.apply(null, array);
+
+    return  max;
+    
+
+} else {
   return 0;
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  
+  if (array && array.length) {
+    
+      var result = array.reduce(function(sum, current) {
+        return sum + current;
+      }, 0);
+
+      var avg = result/array.length;
+
+      return  avg;
+      
+
+  } else {
+    return 0;
+  }
 }
